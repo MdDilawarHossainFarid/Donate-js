a890fba990bb74de5a3607fbe98ebedca60d4788
Md Dilawar Hossain Farid <47987656+MdDilawarHossainFarid@users.noreply.github.com>
2023-09-14 20:48:59 +0530

    example of callback

---
+++ Advanced_JS2/Example of Async Behavior with Callbacks/example3.js
@@ -0,0 +1,12 @@
+console.log("Hello world");
+
+for (let i = 0; i < 3; i++) {
+  setTimeout(function exec() {
+    console.log("Timer done");
+  }, 10);
+}
+
+for (let i = 0; i < 100000000; i++) {
+  // some task
+}
+console.log("End");
